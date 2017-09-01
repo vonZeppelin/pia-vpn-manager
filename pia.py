14955bc7603367d4522aec08046c39a8699a7a4c
Leonid Bogdanov <leonid_bogdanov@mail.ru>
2017-09-02 00:26:33 +1000

    Replace trampoline.sh with script self-spawning

--- pia.py
+++ pia.py
@@ -5,11 +5,11 @@ import http.server as server
 import urllib.request as request
 
 from collections import ChainMap
-from html import escape
 from os import path
 from string import Template
 
-ME_DIR = path.dirname(path.abspath(__file__))
+ME = path.abspath(__file__)
+ME_DIR = path.dirname(ME)
 TMISSION_DAEMON_NAME = "transmission-daemon"
 VPN_DAEMON_NAME = "pia-vpn"
 TMISSION_CFG = "/etc/transmission-daemon/settings.json"
@@ -26,8 +26,8 @@ def start_vpn():
             "--dev-type", "tun",
             "--daemon", VPN_DAEMON_NAME,
             "--script-security", "2",
-            "--up", path.join(ME_DIR, "trampoline.sh \"up\""),
-            "--down", path.join(ME_DIR, "trampoline.sh \"down\""),
+            "--up", ME + " \"up\"",
+            "--down", ME + " \"down\"",
             "--up-restart",
             "--persist-tun"
         ],
@@ -42,14 +42,24 @@ def stop_vpn():
 
 def request_pia_fw_port():
     api_url = "http://209.222.18.222:2000/?client_id=" + PIA_CLIENT_ID
-    with request.urlopen(api_url) as resp:
+    with request.urlopen(api_url, timeout=5) as resp:
         resp_charset = resp.info().get_param('charset') or 'ascii'
         resp_str = resp.read().decode(resp_charset)
         return int(json.loads(resp_str)["port"])
 
 
 def exec_tmission_cmd(cmd):
-    subprocess.run(["/etc/init.d/" + TMISSION_DAEMON_NAME, cmd])
+    subprocess.run(
+        ["/etc/init.d/" + TMISSION_DAEMON_NAME, cmd],
+        stdout=subprocess.DEVNULL
+    )
+
+
+def exec_pia_cmd(cmd):
+    if cmd == "start":
+        start_vpn()
+    elif cmd == "stop":
+        stop_vpn()
 
 
 def update_tmission_settings(bind_addr, bind_port):
@@ -61,7 +71,7 @@ def update_tmission_settings(bind_addr, bind_port):
         settings_file.write(settings)
 
 
-def process_openvpn_cmd(cmd, bind_addr, evt):
+def process_openvpn_evt(cmd, bind_addr, evt):
     if cmd == "up":
         if evt == "init":
             fw_port = request_pia_fw_port()
@@ -75,7 +85,7 @@ def process_openvpn_cmd(cmd, bind_addr, evt):
 
 def start_web_server():
     class RequestHandler(server.BaseHTTPRequestHandler):
-        def log_message(self, format, *args):
+        def log_message(*args):
             pass
 
         def do_GET(self):
@@ -103,10 +113,7 @@ def start_web_server():
             if service == "tmission":
                 exec_tmission_cmd(cmd)
             elif service == "pia":
-                if cmd == "start":
-                    start_vpn()
-                elif cmd == "stop":
-                    stop_vpn()
+                exec_pia_cmd(cmd)
 
             self.send_response(301)
             self.send_header("Location", "/")
@@ -114,10 +121,14 @@ def start_web_server():
 
         @staticmethod
         def _get_process_status(process_name, key_prefix):
-            if subprocess.run(["pgrep", "-f", process_name]).returncode == 0:
-                icon, status = "thumbs-up", "running"
-            else:
+            proc = subprocess.run(
+                ["pgrep", "-f", process_name],
+                stdout=subprocess.DEVNULL
+            )
+            if proc.returncode:
                 icon, status = "thumbs-down", "not running"
+            else:
+                icon, status = "thumbs-up", "running"
             return {
                 key_prefix + "icon": icon,
                 key_prefix + "status": status
@@ -131,21 +142,18 @@ def main():
     argc = len(sys.argv)
     if argc == 1:
         start_web_server()
-        return
     elif argc == 2:
-        cmd = sys.argv[1]
-        if cmd == "start":
-            start_vpn()
-        elif cmd == "stop":
-            stop_vpn()
-        return
+        exec_pia_cmd(sys.argv[1])
+    elif argc == 4:
+        process_openvpn_evt(*sys.argv[1:])
     elif argc == 8:
+        # OpenVPN doesn't allow packets to go through a tunnel until the script
+        # returns with 0 exit code - self-spawn the sript to request a fw port
         cmd, _, _, _, bind_addr, _, evt = sys.argv[1:]
-        process_openvpn_cmd(cmd, bind_addr, evt)
-        return
-
-    print("Illegal arguments specified")
-    sys.exit(1)
+        subprocess.Popen([ME, cmd, bind_addr, evt])
+    else:
+        print("Illegal arguments specified")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
